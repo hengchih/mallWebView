06c48d770b384682e5112e0b13f29760251ab270
chrischang <chrischang@nine-yi.com>
2016-08-30 19:16:39 +0800

    create

---
+++ index.ios.js
@@ -0,0 +1,46 @@
+/**
+ * Sample React Native App
+ * https://github.com/facebook/react-native
+ * @flow
+ */
+
+import React, { Component } from 'react';
+import {
+  AppRegistry,
+  StyleSheet,
+  Text,
+  View,
+  WebView
+} from 'react-native';
+
+class mallWebView extends Component {
+  render() {
+    return (
+        <WebView
+            source={{uri: 'http://amai.91app.tw.dev'}}
+            style={{marginTop: 20}}
+        />
+    );
+  }
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    justifyContent: 'center',
+    alignItems: 'center',
+    backgroundColor: '#F5FCFF',
+  },
+  welcome: {
+    fontSize: 20,
+    textAlign: 'center',
+    margin: 10,
+  },
+  instructions: {
+    textAlign: 'center',
+    color: '#333333',
+    marginBottom: 5,
+  },
+});
+
+AppRegistry.registerComponent('mallWebView', () => mallWebView);
